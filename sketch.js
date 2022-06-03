87f15c5b4c05b489199b078521e521c1c7e48c54
oscaraccorsi <91617877+oscaraccorsi@users.noreply.github.com>
2022-06-03 18:46:59 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let baseUrlPictures = 'https://oscaraccorsi.github.io/pictures/';
+let logo;
 
 let x = [];
 let y = [];
@@ -18,6 +19,18 @@ let inc=20;
 let img;
 let palette = [];
 let palettes = [];
+let pictureList = ['schneider.jpg', 
+                   'schneider01.jpg', 
+                   'schneider01.png', 
+                   'schneider02.jpg',
+                   'schneider03.jpg', 
+                   'schneider04.jpg', 
+                   'schneider05.jpg',  
+                   'schneider06.jpg', 
+                   'schneider07.jpg', 
+                   'schneider08.jpg', 
+                   'schneider09.png', 
+                   'schneider10.png'];
 
 let marginX, marginY;
 let dist = [15, 20, 25, 30, 40, 50];
@@ -31,21 +44,9 @@ let coeffX, coeffY;
 let sec, min, h, day;   
 
 function preload() {
-  palettes[0] = loadImage(baseUrlPictures + 'schneider.jpg');
-  palettes[1] = loadImage(baseUrlPictures + 'schneider01.jpg');
-  palettes[2] = loadImage(baseUrlPictures + 'schneider01.png');
-  palettes[3] = loadImage(baseUrlPictures + 'schneider02.jpg');
-  palettes[4] = loadImage(baseUrlPictures + 'schneider03.jpg');
-  palettes[5] = loadImage(baseUrlPictures + 'schneider04.jpg');
-  palettes[6] = loadImage(baseUrlPictures + 'schneider05.jpg');
-  palettes[7] = loadImage(baseUrlPictures + 'schneider06.jpg'); 
-  palettes[8] = loadImage(baseUrlPictures + 'schneider07.jpg'); 
-  palettes[9] = loadImage(baseUrlPictures + 'schneider08.jpg'); 
-  palettes[10] = loadImage(baseUrlPictures + 'schneider09.png'); 
-  palettes[11] = loadImage(baseUrlPictures + 'schneider10.png');
-  palettes[12] = loadImage(baseUrlPictures + 'schneider11.png');
-  palettes[13] = loadImage(baseUrlPictures + 'schneiderMio.png');
-  palettes[14] = loadImage(baseUrlPictures + 'schneider12.jpg');  
+  h = hour()%12;
+  img = loadImage(baseUrlPictures + pictureList[h]);
+  logo = loadImage(baseUrlPictures + 'good one white.png');  
 }
 
 function windowResized() {
@@ -58,13 +59,13 @@ function setup() {
   background(10); 
   frameRate(fr);
   rectMode(CENTER);
-  h = hour()%12;
+  
   
   min = minute();
   count = round(min/10);
   dst = dist[count];
   
-  img = palettes[h];
+  
   console.log(h, dst);   
   
 
@@ -88,8 +89,15 @@ function setup() {
   marginY = height/10;
   coeffX = width/dst;
   coeffY = height/dst;
-  for (let i = marginX; i < width-marginX; i += (width-marginX*2)/coeffX) {
-    for (let u = marginY; u < height-marginY; u += (height-marginY*2)/coeffY) {
+  
+  for (let i = marginX; 
+       i < width-marginX; 
+       i += (width-marginX*2)/coeffX) {
+    
+    for (let u = marginY; 
+         u < height-marginY; 
+         u += (height-marginY*2)/coeffY) {
+      
       x.push(i);
       y.push(u);
       fill(oneCol);
@@ -108,6 +116,11 @@ function draw() {
   let frameMin = fr*60;
   let time = (frameMin*4)+(frameSec+33);
   
+  if (frameCount >= time) {
+    h = hour()%12;
+    setup(); 
+    frameCount = 0;
+  }
   
   stroke(random(palette));
   chosenPattern();
@@ -143,5 +156,16 @@ function orizontal() {
 
 function keyPressed() {
   clear();
+  h = hour()%12;
   setup();
+}
+
+function mousePressed() {
+  imageMode(CENTER);
+  let xLogo = windowWidth-40;
+  logo.resize(40, 0);
+  image(logo, xLogo, windowHeight-25);
+  tint(200);
+  imageMode(CORNER);
+  save();  
 }
